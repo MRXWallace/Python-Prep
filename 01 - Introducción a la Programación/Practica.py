ea54a6c31b837910f1f020bd5e73863a120daa46
unknown <alansnda@gmail.com>
2022-02-20 14:22:32 -0600

    Second Commit

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,8 @@
+from traceback import print_list
+
+
 print('Hola Mundo!')
+mi_lista = ['Rojo','Azul','Amarillo','Naranja','Violeta','Verde']
+print(mi_lista)
+mi_lista.insert(3,'Negro')
+print(mi_lista)
